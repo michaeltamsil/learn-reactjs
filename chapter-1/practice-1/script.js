26911e06fd18507d17ff13379a8131ea265312e6
michael tamsil <michaeltamsil@gmail.com>
2019-06-23 16:19:13 +0700

    done learn chapter 1

---
+++ chapter-1/practice-1/script.js
@@ -0,0 +1,37 @@
+// result = Math.PI; // result: 3.141592653589793
+// console.log(result);
+
+// result = Math.round(4.7); // result: 5
+// console.log(result);
+
+// result = Math.round(4.4); // result: 4
+// console.log(result);
+
+
+// result = new Date(); // result: [current date]
+// console.log(result);
+// let year = result.getFullYear(); //year: 2018
+
+
+// let bigValue = numeral('1,000,000,000,200.00');
+//   let integerValue = bigValue.value();
+//   console.log(integerValue);
+//   console.log('the value is ',numeral(integerValue).format('$ 0,0.00'));
+
+//   let secondValue = numeral('$ 200,500,000');
+//   console.log('second\'s value is ', secondValue.value());
+
+
+//   moment.locale('id');
+//   let today = moment();
+//   console.log('today is ', today.format('dddd, MMMM Do YYYY'))
+
+//   console.log('next week is ',moment().add(1, 'weeks').format('DD MMMM YYYY'))  
+
+
+let textNumber = "";
+for (let counter = 0; counter < 5; counter++) {
+  textNumber += counter + ",";
+  0/1;
+}
+console.log(textNumber)
